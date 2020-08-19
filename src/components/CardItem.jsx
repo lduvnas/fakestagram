56f418532e65c1ef4e29e1d1f34fc1e2af47b67e
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-08-19 15:21:49 +0200

    Show Description function

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 import LikeButton from "./LikeButton";
+import { useState } from "react";
 
 export default function CardItem({ title, description, imageURL }) {
+  let [showDescription, setShowDescription] = useState(false);
+
+  function renderDescription() {
+    if (showDescription) {
+      return <p className="card-text">{description}</p>;
+    } else {
+      return "";
+    }
+  }
+
+  function renderShowMore() {
+    if (showDescription === false) {
+      return (
+        <p>
+          <a onClick={() => setShowDescription(true)}>read more</a>
+        </p>
+      );
+    } else {
+      return "";
+    }
+  }
+
   return (
     <div className="col-md-3">
       <div className="card text-center">
         <img className="card-img-top" src={imageURL} alt={imageURL} />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
-          <p className="card-text ">{description}</p>
+          {renderShowMore()}
+          {renderDescription()}
+
           <a className="text-center" href={imageURL}>
             Download image
           </a>
