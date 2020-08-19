bc3ea8d4e40f2641f97ce8b6c51c302d7d961816
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-08-19 14:35:03 +0200

    fetch images

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import CardItem from "./CardItem";
-export default function CardList({ data }) {
+import { useState, useEffect } from "react";
+
+export default function CardList() {
+  let [imageList, setImageList] = useState([]);
+
+  function fetchImageList() {
+    fetch("https://image-mock-data.firebaseio.com/images.json")
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+        setImageList(result);
+      });
+  }
+
+  useEffect(() => {
+    fetchImageList();
+  }, []);
+
   return (
     <div className="card-box row">
-      {data.map((item, index) => {
+      {imageList.map((item, index) => {
         return (
           <CardItem
             title={item.title}
